a26a3705ce4fc2c55d73c4b223a16afcb3a251e0
xiaohe-4 <2672985170@qq.com>
2026-09-13 10:56:53 +0800

    fix error

--- huicha-aml/start.bat
+++ huicha-aml/start.bat
@@ -1,6 +1,7 @@
 @echo off
+set "PYTHON=%~dp0.venv\Scripts\python.exe"
 cd /d "%~dp0backend"
-start "循证慧查-API" cmd /k py -m uvicorn app.main:app --reload --port 8000
+start "循证慧查-API" cmd /k ""%PYTHON%" -m uvicorn app.main:app --reload --port 8000"
 cd /d "%~dp0frontend"
 start "循证慧查-WEB" cmd /k npm run dev
 echo 浏览器打开 http://127.0.0.1:5173
